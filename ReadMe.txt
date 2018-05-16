a035328b7fba6fbb318926e680e8e147dec93147
yangziyi1990 <yangziyi091100@163.com>
2018-05-16 14:22:52 +0800

    Add file

---
+++ ReadMe.txt
@@ -0,0 +1,7 @@
+This code is written by Ziyi according to the reference "L1 / 2 Regularized Logistic Regression 2012 (Q.zhao,DY.Meng,ZB.Xu)"
+combine mutiple simulation datasets for meta-analysis
+
+Step1£ºGenerating multiple simulation datasets (using Combat)£»
+Step2£ºGenerating dataset such as "data_combat.txt" and "Datasety.txt"£»
+Step3£ºImport "data_combat.txt" and "Datasety.txt" into MATLAB£»
+Step4£ºRun script "Logistic_***_sim_multidata.m" for feature selection and classification.
---
+++ simulation_combat.R
@@ -0,0 +1,13 @@
+setwd("D:\\Ziyi\\School\\PMO\\metanalysis\\Simulation\\combine\\data")
+
+data1<-read.table("Dataset1_series_matrix.txt",sep="\t",header=T,row.names=1)
+data2<-read.table("Dataset2_series_matrix.txt",sep="\t",header=T,row.names=1)
+data3<-read.table("Dataset3_series_matrix.txt",sep="\t",header=T,row.names=1)
+sample<-read.table("Datasety.txt",sep = "\t",header = T)
+
+dat<-cbind(data1,data2,data3)
+rownames(sample)<-sample[,1]
+
+# Combat
+data_combat<-ComBat(dat, batch=sample[,2],mod=NULL, par.prior=TRUE, prior.plots=TRUE)
+write.table(data_combat,file="data_combat.txt",sep="\t")
